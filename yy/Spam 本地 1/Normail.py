1f6f2e1211fe82d1c4bef0be971c9169c756e1da
Lemonyy-13 <45934234+Lemonyy-13@users.noreply.github.com>
2019-09-03 19:46:52 +0800

    更新主界面UI（本地版本）
    在本地版本上进行主界面UI美化（已更新到服务器版本）

---
+++ yy/Spam 本地 1/Normail.py
@@ -0,0 +1,190 @@
+import sys
+
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
+from PyQt5.QtGui import *
+import Infor
+import Lgmail
+import NewSign
+import pickle
+
+
+class DownLoadThread(QThread):
+
+    def __init__(self, im, id, mails,btn):
+        super().__init__()
+        self.im = im
+        self.result = list()
+        self.id = id
+        self.mails = mails
+        self.btn = btn
+
+    def run(self):
+        self.im.get_attach(self.mails['正常邮件'][self.id]['邮件'])
+        self.btn.setText("完成")
+
+class NormailUi(QDialog):
+    def __init__(self, im, mail):
+        super(NormailUi, self).__init__()
+        self.mails = mail
+        self.im = im
+        self.initUI()
+        self.download = None
+
+    def initUI(self):
+        self.setWindowTitle("正常邮件")
+        self.resize(680, 527)
+        layout = QHBoxLayout()
+        self.setWindowOpacity(0.9)
+        self.setStyleSheet('''
+                        QDialog{
+                                background:#F5F5F5;
+                                font-family: "Microsoft Yahei";}         
+                        ''')
+
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.horizontalHeader().setVisible(False)
+        self.tableWidget.setShowGrid(False)
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        # self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.tableWidget.setColumnWidth(0, 150)
+        self.tableWidget.setColumnWidth(1, 250)
+        self.tableWidget.setColumnWidth(2, 85)
+        self.tableWidget.setColumnWidth(3, 85)
+        self.tableWidget.setColumnWidth(4, 85)
+        self.tableWidget.setStyleSheet('''
+                            QTableWidget{
+                                        text-align: center;
+                                        background: transparent;
+                                        font-family: SimHei;}            
+                            ''')
+
+        self.row_count = 0
+        i = 0
+        for each in self.mails['正常邮件']:
+            self.addLine(each['发件人'], each['主题'], i)
+            i += 1
+        layout.addWidget(self.tableWidget)
+        self.setLayout(layout)
+
+    #### 动态添加一行信息
+    def addLine(self, name, text, id):
+        # 添加一空白行
+        self.row_count = self.tableWidget.rowCount()
+        self.tableWidget.insertRow(self.row_count)
+
+        # 发件人
+        sender = QLabel()
+        sender.setFocusPolicy(QtCore.Qt.NoFocus)
+        sender.setText(name)
+        sender.setToolTip(name)
+        sender.setFont(QFont("Segoe UI"))
+        # sender.setStyleSheet('''
+        #                     QLabel{
+        #                         dont-family:"Segoe UI";
+        #                         background:white;
+        #                         border:none;}
+        #                         ''')
+
+        # 邮件内容
+        infor = QLineEdit()
+        infor.setFocusPolicy(QtCore.Qt.NoFocus)
+        infor.setText(text)
+        infor.setStyleSheet('''
+                            QLineEdit{
+                                color:#4F4F4F;
+                                font-family:"Segoe UI";
+                                background: transparent;
+                                border:none;}
+                                ''')
+
+        # 打开按钮
+        # print(self.mails['正常邮件'][id-1])
+        openBtn = QPushButton("打开")
+        openBtn.clicked.connect(lambda: self.onBtnOpen(id))
+        btStlSheet = '''QPushButton
+                                         {text-align : center;
+                                         background-color : white;
+                                         font: bold;
+                                         font-family: "Microsoft Yahei";
+                                         border-color: gray;
+                                         border-width: 2px;
+                                         border-radius: 10px;
+                                         padding: 6px;
+                                         height : 14px;
+                                         border-style: outset;
+                                         font : 14px;}
+                                         QPushButton:pressed
+                                         {text-align : center;
+                                         background-color : light gray;
+                                         font: bold;
+                                         border-color: gray;
+                                         border-width: 2px;
+                                         border-radius: 10px;
+                                         padding: 6px;
+                                         height : 14px;
+                                         border-style: outset;
+                                         font : 14px;}'''
+        openBtn.setStyleSheet(btStlSheet)
+
+        # 关闭按钮
+        delBtn = QPushButton("删除")
+        row = self.row_count
+        # delBtn.clicked.connect(lambda: self.onBtnDel(row))
+        delBtn.setStyleSheet(btStlSheet)
+
+        # 下载按钮
+        dlBtn = QPushButton("下载附件")
+        dlBtn.clicked.connect(lambda: self.onBtnDL(id))
+        if not self.mails['正常邮件'][id]['附件']:
+            dlBtn.setEnabled(False)
+        dlBtn.setStyleSheet(btStlSheet)
+
+
+        delBtn.clicked.connect(lambda: self.onBtnDel(id))
+
+        # self.tableWidget.setItem(self.row_count, 0, QTableWidgetItem(name))
+        self.tableWidget.setCellWidget(self.row_count, 0, sender)
+        self.tableWidget.setCellWidget(self.row_count, 1, infor)
+        self.tableWidget.setCellWidget(self.row_count, 2, openBtn)
+        self.tableWidget.setCellWidget(self.row_count, 3, delBtn)
+        self.tableWidget.setCellWidget(self.row_count, 4, dlBtn)
+
+    def onBtnDL(self, id):
+        Btn = self.sender()
+        Btn.setEnabled(False)
+        Btn.setText("正在下载...")
+        self.download = DownLoadThread(self.im, id, self.mails, Btn)
+        self.download.start()
+
+    def onBtnOpen(self, id):
+        dialog = Infor.InforUi(self.mails['正常邮件'][id])
+        dialog.exec()
+
+    def onBtnDel(self, id):
+        button = self.sender()
+        if button:
+            r = self.tableWidget.indexAt(button.pos()).row()
+            row = int(r)
+            self.tableWidget.removeRow(row)
+            self.row_count = self.row_count - 1
+            # self.im.delete_mail(self.mails['正常邮件'][id]['邮件'])
+
+            for each in self.mails['正常邮件'][id + 1:]:
+                each['邮件'] -= 1
+            del self.mails['正常邮件'][id]
+
+    def closeEvent(self, event):
+        # pickle_file = open('received_mails.pkl', 'wb')  # 存储到本地文件
+        # pickle.dump(self.mails, pickle_file)
+        # pickle_file.close()
+        pass
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    dialag = NormailUi()
+    dialag.show()
+    sys.exit(app.exec_())
---
+++ yy/Spam 本地 1/filter2.py
@@ -0,0 +1,133 @@
+from spamEmail import spamEmailBayes
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import wordnet
+
+# spam类对象
+spam = spamEmailBayes()
+# 停用词
+stopList = spam.getStopWords()
+# 获取训练集中正常邮件与垃圾邮件的数量
+normFilelen = 7063
+spamFilelen = 7775
+
+
+# 正常词汇频率 垃圾邮件词汇频率
+def Get_Dict(normal_email, spam_email):
+    normDict = {}
+    spamDict = {}
+    with open(normal_email, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            k1 = line.split(' ')[0]
+            v1 = int(line.split(' ')[1])
+            normDict[k1] = v1
+    with open(spam_email, 'r') as f:
+        for line in f.readlines():
+            line = line.strip()
+            k2 = line.split(' ')[0]
+            v2 = int(line.split(' ')[1])
+            spamDict[k2] = v2
+    return normDict, spamDict
+
+
+# 获取邮件内容
+def Get_Email(fileName):
+    with open(fileName, 'r') as f:
+        Email_Content = f.read()
+    return Email_Content
+
+
+# 获取邮件分词后,然后统计词频的成果
+def Get_Email_Div(content):
+    wordsList = []
+    rule = re.compile(r"[^\u4e00-\u9fa5]")
+    line = rule.sub("", content)
+    spam.get_word_list(line, wordsList, stopList)
+    testDict = {}
+    spam.addToDict(wordsList, testDict)
+    return testDict
+
+
+def Get_Bayes_Num(testDict, spamDict, normDict, normFilelen, spamFilelen):
+    wordProbList = spam.getTestWords(testDict, spamDict, normDict, normFilelen, spamFilelen)
+    p = spam.calBayes(wordProbList, spamDict, normDict)
+    if p > 0.9:
+        return 1
+    else:
+        return 0
+
+
+# add English Bayes
+def Filter_text(text):
+    str = re.sub('[^a-zA-Z]', ' ', text)
+    str = re.sub(r'\s+', ' ', str)
+    s = str.split(" ")
+    num = 0
+    for i in s:
+        if wordnet.synsets(i):
+            num += len(i)
+    return str.lower(), num
+
+
+# 统计垃圾邮件和健康邮件的词频
+def Count(text):
+    vectorizer = CountVectorizer()
+    L = ['']
+    L[0] = text
+    weight = vectorizer.fit_transform(L).toarray()
+    word = vectorizer.get_feature_names()  # 所有文本的关键字
+    return {word[j]: int(weight[0][j]) for j in range(len(word))}
+
+
+# 求词频字典的总频数
+def Sum(dic):
+    n = 0
+    for value in dic.values():
+        n = n + value
+    return n
+
+
+def Bayes(test, num, health_dic, spam_dic, health_sum, spam_sum):
+    test, numc = Filter_text(test)
+    test_count = sorted(Count(test).items(), key=lambda x: x[1], reverse=True)
+    # 提取前15个词作计算条件概率，代入贝叶斯联合公式
+    # 如果长度不够，就取总词数
+    if len(test_count) >= 15:
+        r = 15
+    else:
+        r = len(test_count)
+    P = []
+    for n in range(r):
+        word = test_count[n][0]
+        if not spam_dic.get(word):
+            P.append(0.4)
+        # 如果有的词是第一次出现,无法计算P(S | W),就假定这个值等于0.4。
+        # 因为垃圾邮件用的往往都是某些固定的词语，所以如果你从来没见过某个词，它多半是一个正常的词。
+        elif not health_dic.get(word):
+            word_ham = 0.003
+            # 这个值可能还需要修正,资料中给出的值是1%
+            # 如果某个词只出现在垃圾邮件中, 就假定，它在正常邮件的出现频率是0.3 %
+            word_spam = spam_dic[word] / spam_sum
+            P.append((word_spam * 0.5) / ((word_ham * 0.5) + (word_spam * 0.5)))
+
+        else:
+            word_spam = spam_dic[word] / spam_sum
+            word_ham = health_dic[word] / health_sum
+            P.append((word_spam * 0.5) / ((word_ham * 0.5) + (word_spam * 0.5)))
+    # 计算联合概率
+    p1 = 1
+    p2 = 1
+    for n in range(r):
+        p1 = p1 * P[n]
+        p2 = p2 * (1 - P[n])
+    p = (p1 / (p1 + p2))
+
+    if (numc / num) < 0.2:
+        p = 1
+
+    if p > 0.5:
+        pc = 1
+    else:
+        pc = 0
+    return pc
